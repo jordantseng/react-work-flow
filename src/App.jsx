8be01e0a3ea6784b79ae7c4ee22b87c52c091f9f
Jordan Tseng <jordantseng1024@gmail.com>
2022-11-23 16:52:52 +0800

    after formatted

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-
-
-
   return (
-    
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
