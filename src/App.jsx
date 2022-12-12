0a62977ec4dafc96d79bf1597b5db69be3ac39d5
Jordan Tseng <jordantseng@ns-guard.com>
2022-12-12 10:17:55 +0800

    feat: validate test cases

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Reac</h1>
       <div className="card">
         <button type="button" onClick={() => setCounter((prevCount) => prevCount + 1)}>
           count is {counter}
