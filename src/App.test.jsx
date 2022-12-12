f660d3ee2479fea4bc51257e9962692098cbcd32
Jordan Tseng <jordantseng@ns-guard.com>
2022-12-12 10:14:17 +0800

    feat: setup react-testing-library

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '../test-utils';
+import App from './App';
+
+describe('Simple working test', () => {
+  it('the title is visible', () => {
+    render(<App />);
+
+    const welcomeText = screen.getByText(/Vite \+ React/i);
+
+    screen.debug(welcomeText);
+
+    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+  });
+});
